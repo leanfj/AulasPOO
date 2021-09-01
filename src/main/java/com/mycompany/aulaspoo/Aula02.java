bc1a45a1479725c2a60b2ed2ba11702c0fc81179
leanfj <leanfj@hotmail.com>
2021-08-31 23:18:53 -0300

    Primeiro commit classe principal

---
+++ src/main/java/com/mycompany/aulaspoo/Aula02.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.aulaspoo;
+
+/**
+ *
+ * @author Locatech
+ */
+public class Aula02 {
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta();
+        
+        c1.modelo = "Bic";
+        c1.cor = "Azul";
+        c1.ponta = 0.5f;
+        c1.carga = 85;
+        c1.tampada = true;
+        
+        c1.destampar();
+        c1.status();
+        
+        c1.rabiscar();
+        
+        c1.tampar();
+        c1.status();
+        c1.rabiscar();
+        
+    }
+}
