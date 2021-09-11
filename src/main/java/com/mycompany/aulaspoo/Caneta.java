65892b7e9c88fa64ec74d9b7d8f5d73854427c47
leanfj <leanfj@hotmail.com>
2021-09-11 17:32:40 -0300

    Alteração de modificadores de acesso da classe caneta

--- src/main/java/com/mycompany/aulaspoo/Caneta.java
+++ src/main/java/com/mycompany/aulaspoo/Caneta.java
@@ -1,52 +1,43 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.aulaspoo;
 
-/**
- *
- * @author Locatech
- */
 public class Caneta {
-    String modelo;
-    String cor;
-    float ponta;
-    int carga;
-    boolean tampada;
-    
-    void status(){
+
+    public String modelo;
+    public String cor;
+    private float ponta;
+    protected int carga;
+    private boolean tampada;
+
+    public void status() {
         System.out.println("Modelo: " + this.modelo);
         System.out.println("Cor: " + this.cor);
         System.out.println("Ponta: " + this.ponta);
         System.out.println("Carga: " + this.carga);
-        
-        if(this.tampada) {
+
+        if (this.tampada) {
             System.out.println("Esta caneta está tampada!");
         } else {
             System.out.println("Esta caneta está destampada");
         }
-        
+
     }
-    
-    void rabiscar() {
-        
+
+    public void rabiscar() {
+
         if (!this.tampada) {
             System.out.println("Estou rabiscando");
         } else {
             System.out.println("Erro! Não posso rabiscar!");
         }
-                
-        
+
     }
-    
-    void tampar() {
+
+    public void tampar() {
         this.tampada = true;
-        
+
     }
-    
-    void destampar() {
+
+    public void destampar() {
         this.tampada = false;
     }
 }
